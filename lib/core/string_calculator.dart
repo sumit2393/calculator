17578b2c2122f0f933ce4581cd4fa4bbc63b0b83
sumit2393 <appsumit0@gmail.com>
2026-01-07 13:55:40 +0530

    logic for TDD custom & newline  delimiter

--- lib/core/string_calculator.dart
+++ lib/core/string_calculator.dart
@@ -2,5 +2,19 @@ int add(String numbers) {
   if (numbers.isEmpty) {
     return 0;
   }
-  return int.parse(numbers);
+  String delimiter = ',';
+  String numbersPart = numbers;
+  // Handle custom delimiter
+  if (numbers.startsWith('//')) {
+    final lines = numbers.split('\n');
+    delimiter = lines[0].substring(2);
+    numbersPart = lines[1];
+  }
+
+  // Support newline delimiter
+  numbersPart = numbersPart.replaceAll('\n', delimiter);
+
+  final values = numbersPart.split(delimiter).map((e) => int.parse(e)).toList();
+
+  return values.reduce((a, b) => a + b);
 }
