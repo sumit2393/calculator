1f118f9f0bf32f01aff5dc70a922fbe6f78804b9
sumit2393 <appsumit0@gmail.com>
2026-01-07 14:02:39 +0530

    adding TDD only for negative and multiple negative number

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -10,5 +10,42 @@ void main() {
     test('Single number should return the number', () {
       expect(add("1"), equals(1));
     });
+
+    test('Two numbers separated by comma', () {
+      expect(add("1,5"), equals(6));
+    });
+
+    test('Multiple numbers', () {
+      expect(add("1,2,3,4"), equals(10));
+    });
+
+    test('New line as delimiter', () {
+      expect(add("1\n2,3"), equals(6));
+    });
+
+    test('Custom delimiter', () {
+      expect(add("//;\n1;2"), equals(3));
+    });
+
+    // Negative numbers should throw exception
+    expect(
+      () => add("1,-2"),
+      throwsA(
+        predicate(
+          (e) => e.toString().contains("negative numbers not allowed -2"),
+        ),
+      ),
+    );
+  });
+  // Additional test for multiple negative numbers
+  test('Multiple negative numbers should show all', () {
+    expect(
+      () => add("1,-2,-3"),
+      throwsA(
+        predicate(
+          (e) => e.toString().contains("negative numbers not allowed -2,-3"),
+        ),
+      ),
+    );
   });
 }
